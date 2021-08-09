91a3e0e1523c9592391a30c27e5ffeb5823b7f38
kth4249 <desales4249@gmail.com>
2021-08-09 14:58:50 +0900

    함수 타입 정의

--- ts-practice/src/practice.ts
+++ ts-practice/src/practice.ts
@@ -1,19 +1,21 @@
-let count = 0; // 숫자
-count += 1;
-count = '갑자기 분위기 문자열'; // 이러면 에러가 납니다!
+// 함수에서 타입 정의하기
 
-const message: string = 'hello world'; // 문자열
+//                                  반환형 타입
+function sum(x: number, y: number): number {
+    return x + y;
+    // return null; // 오류 발생!
+}
 
-const done: boolean = true; // 불리언 값
+sum(1, 2);
 
-const number: number[] = [1, 2, 3]; // 숫자 배열
-const messages: string[] = ['hello', 'world']; // 문자열 배열
+function sumArray(numbers: number[]): number {
+    // 배열의 내장함수를 사용 할 때에도 타입 유추가 잘 이루어짐
+    return numbers.reduce((acc, current) => acc + current, 0);
+}
 
-message.push(1); // 숫자 넣으려고 하면.. 안된다!
+const total = sumArray([1, 2, 3, 4, 5]);
 
-let mightBeUnderfined: string | undefined = undefined; // string 일수도 잇고 undefined 일수도 있음.
-let nullableNumber: number | null = null; // number 일수도 있고 null 일수도 있음.
-
-let color: 'red' | 'orange' | 'yellow' = 'red'; // red, orange, yellow 중 하나임
-color = 'yellow';
-color = 'green' // 에러 발생!
+// 함수가 아무것도 반환하지 않아야 할 때
+function returnNothing(): void {
+    console.log('I am just saying hello world');
+}
