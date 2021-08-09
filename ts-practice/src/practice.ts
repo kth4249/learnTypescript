2b6f525a2c9b390bc625b49b919ceb916748ab46
kth4249 <desales4249@gmail.com>
2021-08-09 14:48:34 +0900

    기본 타입 정의

--- ts-practice/src/practice.ts
+++ ts-practice/src/practice.ts
@@ -1,2 +1,19 @@
-const message: string = 'hello world';
-console.log(message);
+let count = 0; // 숫자
+count += 1;
+count = '갑자기 분위기 문자열'; // 이러면 에러가 납니다!
+
+const message: string = 'hello world'; // 문자열
+
+const done: boolean = true; // 불리언 값
+
+const number: number[] = [1, 2, 3]; // 숫자 배열
+const messages: string[] = ['hello', 'world']; // 문자열 배열
+
+message.push(1); // 숫자 넣으려고 하면.. 안된다!
+
+let mightBeUnderfined: string | undefined = undefined; // string 일수도 잇고 undefined 일수도 있음.
+let nullableNumber: number | null = null; // number 일수도 있고 null 일수도 있음.
+
+let color: 'red' | 'orange' | 'yellow' = 'red'; // red, orange, yellow 중 하나임
+color = 'yellow';
+color = 'green' // 에러 발생!
